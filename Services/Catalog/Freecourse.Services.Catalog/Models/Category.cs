9c7c519c8e00977e38b3386cc926c9853e7e1508
Kerem DURAK <kerem.durak@yahoo.com>
2023-08-25 19:40:25 +0300

    category interface and service added

--- Services/Catalog/Freecourse.Services.Catalog/Models/Category.cs
+++ Services/Catalog/Freecourse.Services.Catalog/Models/Category.cs
@@ -1,10 +1,13 @@
-﻿using MongoDB.Bson;
+﻿using Freecourse.Services.Catalog.Settings;
+using Microsoft.Extensions.Options;
+using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 
 namespace Freecourse.Services.Catalog.Models
 {
     public class Category
     {
+       
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
         public string Id { get; set; }
---
+++ Services/Catalog/Freecourse.Services.Catalog/Services/ICategoryService.cs
@@ -0,0 +1,15 @@
+﻿using Freecourse.Services.Catalog.Dtos;
+using Freecourse.Services.Catalog.Models;
+using FreeCourse.Shared.Dtos;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Freecourse.Services.Catalog.Services
+{
+    public interface ICategoryService
+    {
+        Task<Response<List<CategoryDto>>> GetAllAsync();
+        Task<Response<CategoryDto>> CreateAsync(Category category);
+        Task<Response<CategoryDto>> GetByIdAsync(string id);
+    }
+}
