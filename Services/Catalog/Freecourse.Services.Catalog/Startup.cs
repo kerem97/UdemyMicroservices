da5ae9ef8763fc96143169e6157e325960f8e0f3
Kerem DURAK <kerem.durak@yahoo.com>
2023-08-27 00:36:01 +0300

    Jwt Access Token

--- Services/Catalog/Freecourse.Services.Catalog/Startup.cs
+++ Services/Catalog/Freecourse.Services.Catalog/Startup.cs
@@ -1,8 +1,10 @@
 using Freecourse.Services.Catalog.Services;
 using Freecourse.Services.Catalog.Settings;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -28,12 +30,23 @@ namespace Freecourse.Services.Catalog
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+            {
+                options.Authority = Configuration["IdentityServerURL"];
+                options.Audience = "resource_catalog";
+                options.RequireHttpsMetadata = false;
+
+            });
+
+
+
             services.AddScoped<ICategoryService, CategoryService>();
             services.AddScoped<ICourseService, CourseService>();
-
-
             services.AddAutoMapper(typeof(Startup));
-            services.AddControllers();
+            services.AddControllers(opt =>
+            {
+                opt.Filters.Add(new AuthorizeFilter());
+            });
 
 
             services.Configure<DatabaseSettings>(Configuration.GetSection("DatabaseSettings"));
@@ -51,6 +64,12 @@ namespace Freecourse.Services.Catalog
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Freecourse.Services.Catalog", Version = "v1" });
             });
+
+
+
+
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -64,7 +83,7 @@ namespace Freecourse.Services.Catalog
             }
 
             app.UseRouting();
-
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
